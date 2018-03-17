607c2515a4ba3b0da43a28871af2f6d9d870c266
Alexei Shelestov <iikebot@gmail.com>
2018-03-17 11:31:30 +0300

    Added Chef

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+node.default['test']['address'] = '192.168.0.10:5000'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,16 @@
 #
 # Cookbook:: test
 # Recipe:: default
-#
+puts node.default
+package 'install docker' do
+    package_name 'docker'
+end
+
+file '/etc/docker/daemon.json' do
+    owner 'root'
+    group 'root'
+    mode '644'
+    content "{ \"insecure-registries\" : [\"#{node['test']['address']}\"] }"
+    action :create
+end
 # Copyright:: 2018, The Authors, All Rights Reserved.
